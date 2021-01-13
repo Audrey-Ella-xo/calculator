7b86137f15f186d34d9d63d99bbba0f80bef8791
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-01-13 09:54:01 +0100

    Remove logo and test files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 // import PropTypes from 'prop-types';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
           {' '}
