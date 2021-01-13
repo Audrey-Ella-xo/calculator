88ea044c7b0e6d0572700135cd1bfb09f3c26793
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-01-13 14:00:15 +0100

    Implement ButtonPannel

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-    const { name } = props;
-    return (
-        <>
-            <button type = 'button'>
-                {name}
-            </button>
-        </>
-    )
-}
+  const { name } = props;
+  return (
+    <>
+      <button type="button">
+        {name}
+      </button>
+    </>
+  );
+};
 
-Button.propTypes = {
-    name: PropTypes.string,
-}
+Button.propTypes = { name: PropTypes.string.isRequired };
 
 export default Button;
