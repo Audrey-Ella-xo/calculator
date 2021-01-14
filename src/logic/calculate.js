7be72e01a39a8c6ee9cb922e02839d3a244b0525
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-01-14 12:01:32 +0100

    fix lint errors

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,61 +1,59 @@
-import operate from './operate'
+import operate from './operate';
 
 const Calculate = (data, btnName) => {
-    let { total, next, operation } = data;
+  let { total, next, operation } = data;
 
-    switch (btnName) {
-        case 'AC':
-            total = null;
-            next = null;
-            operation = null
-            break;
-        case '+':
-        case '-':
-        case 'x':
-        case '÷':
-        case '%':
-            if(!total) break;
-            if(next && operation) {
-                total = operate(total, next, operation);
-            }
-            operation = btnName;
-            break;
-        case '.':
-            if (total && !total.split('').includes('.') && !operation ){
-                total = `${total}${btnName}`
-            }
-            else if(next){
-                next += btnName;
-            }
-            else {
-                next = `0${btnName}`
-            }
-            break;
-        case '=':
-            if(total && next){
-                total = operation(total, next, operation);
-                next = null
-                operation = null
-            }
-            break;
-        case '+/-':
-            if(total){
-                total *= -1;
-            } else if(next){
-                next *= -1;
-            }
-            break;
+  switch (btnName) {
+    case 'AC':
+      total = null;
+      next = null;
+      operation = null;
+      break;
+    case '+':
+    case '-':
+    case 'x':
+    case '÷':
+    case '%':
+      if (!total) break;
+      if (next && operation) {
+        total = operate(total, next, operation);
+      }
+      operation = btnName;
+      break;
+    case '.':
+      if (total && !total.split('').includes('.') && !operation) {
+        total = `${total}${btnName}`;
+      } else if (next) {
+        next += btnName;
+      } else {
+        next = `0${btnName}`;
+      }
+      break;
+    case '=':
+      if (total && next) {
+        total = operation(total, next, operation);
+        next = null;
+        operation = null;
+      }
+      break;
+    case '+/-':
+      if (total) {
+        total *= -1;
+      } else if (next) {
+        next *= -1;
+      }
+      break;
 
-        default:
-            if (next){
-                next += btnName;
-            }else {
-                next = btnName;
-            }
-            break;
-    }
+    default:
+      if (next) {
+        next += btnName;
+      } else {
+        next = btnName;
+      }
+      break;
+  }
 
-    return { total, next, operation }
-}
+  return { total, next, operation };
+};
 
 export default Calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,32 +1,32 @@
 import Big from 'big.js';
 
 const Operate = (numOne, numTwo, operation) => {
-    const num1 = Big(numOne);
-    const num2 = Big(numTwo);
+  const num1 = Big(numOne);
+  const num2 = Big(numTwo);
 
-    let total;
+  let total;
 
-    switch (operation) {
-        case '+':
-            total = num1.plus(num2)
-            break;
-        case '-':
-            total = num1.minus(num2)
-            break;
-        case 'x':
-            total = num1.times(num2)
-            break;
-        case '÷':
-            total = num1 == 0 ? 'Error: Not divisible by zero' : num1.div(num2)
-            break;
-        case '%':
-            total = num1.div(100)
-            break;       
-        default:
-            break;
-    }
+  switch (operation) {
+    case '+':
+      total = num1.plus(num2);
+      break;
+    case '-':
+      total = num1.minus(num2);
+      break;
+    case 'x':
+      total = num1.times(num2);
+      break;
+    case '÷':
+      total = num1 === 0 ? 'Error: Not divisible by zero' : num1.div(num2);
+      break;
+    case '%':
+      total = num1.div(100);
+      break;
+    default:
+      break;
+  }
 
-    return total;
-}
+  return total;
+};
 
-export default Operate
+export default Operate;
