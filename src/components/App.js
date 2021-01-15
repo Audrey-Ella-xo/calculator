430894705f1252abaeb0ff442734e1fe0d397d65
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-01-15 09:29:41 +0100

    Convert App component to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
 // import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <>
-        <div className="calc-container">
-          <Display />
-          <ButtonPannel />
-        </div>
-      </>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: '0',
+    };
+  }
+
+  render() {
+    const { result } = this.state;
+    return (
+      <div className="App">
+        <>
+          <div className="calc-container">
+            <Display result={result} />
+            <ButtonPannel />
+          </div>
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
