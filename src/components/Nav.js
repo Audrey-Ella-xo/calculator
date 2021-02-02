d1e0139c02c6a48d79707a875c7a431c82d91d52
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-02-02 22:46:09 +0100

    add links to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <nav>
-    <ul>
-      <li>Home</li>
-      <li>Calculator</li>
-      <li>Quotes</li>
+    <ul className="nav-links">
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/calculator">
+        <li>Calculator</li>
+      </Link>
+      <Link to="/quotes">
+        <li>Quotes</li>
+      </Link>
     </ul>
   </nav>
 );
--- src/components/Router.js
+++ src/components/Router.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import App from './App';
 import Quotes from './Quotes';
+import Nav from './Nav';
 
 const Routes = () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/calculator" exact component={App} />
-      <Route path="/quotes" exact component={Quotes} />
-    </Switch>
+    <div>
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/calculator" exact component={App} />
+        <Route path="/quotes" exact component={Quotes} />
+      </Switch>
+    </div>
   </Router>
 );
 
