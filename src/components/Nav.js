022769d80c9f61f1c38bb59c2a08c05a34a94458
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-02-02 21:44:43 +0100

    Install React router and add files for math magicians

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Calculator</li>
+                <li>Quotes</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
